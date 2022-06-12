6bcdf45343f74654af36fc8ce089d2f789a066e9
SaulReznik <robhov97@gmail.com>
2022-06-12 21:59:15 +0400

    done extention video

---
+++ youtube-extension/contentScript.js
@@ -0,0 +1,70 @@
+(() => {
+    let youtubeLeftControls;
+    let youtubePlayer;
+    let currentVideo = '';
+    let currentVideoBookmarks = [];
+
+    const fetchBookmarks = () => {
+        return new Promise((resolve) => {
+            chrome.storage.sync.get([currentVideo], obj => {
+                resolve(obj[currentVideo] ? JSON.parse(obj[currentVideo]) : []);
+            });
+        });
+    }
+    
+    const getTime = t => {
+        const date = new Date(0);
+        date.setSeconds(t);
+
+        return date.toISOString().slice(11, 19);
+    };
+
+    const addNewBookmarkEventHandler = async () => {
+        const { currentTime } = youtubePlayer;
+        const newBookmark = {
+            time: currentTime,
+            desc: `Bookmark at ${getTime(currentTime)}`
+        };
+
+        currentVideoBookmarks = await fetchBookmarks();
+
+        chrome.storage.sync.set({
+            [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time)),
+        })
+    };
+
+    const newVideoLoaded = async () => {
+        const doesBookmarkBtnExists = document.getElementsByClassName('bookmar-btn')[0];
+        currentVideoBookmarks = await fetchBookmarks();
+
+        if(!doesBookmarkBtnExists) {
+            const bookmarkBtn = document.createElement('img');
+
+            bookmarkBtn.src = chrome.runtime.getURL('assets/bookmark.png');
+            bookmarkBtn.className = 'ytp-button bookmark-btn';
+            bookmarkBtn.title = 'Click to bookmark current timestamp';
+
+            youtubeLeftControls = document.getElementsByClassName('ytp-left-controls')[0];
+            youtubePlayer = document.getElementsByClassName('video-stream')[0];
+        
+            youtubeLeftControls.appendChild(bookmarkBtn);
+            bookmarkBtn.addEventListener('click', addNewBookmarkEventHandler);
+        }
+    };
+
+    chrome.runtime.onMessage.addListener((obj, sender, response) => {
+        const { type, value, videoId } = obj;
+    
+        if (type === 'NEW') {
+            currentVideo = videoId;
+            newVideoLoaded();
+        } else if (type === 'PLAY') {
+            youtubePlayer.currentTime = value;
+        } else if (type === 'DELETE') {
+            currentVideoBookmarks = currentVideoBookmarks.filter(b => b.time != value);
+            chrome.storage.sync.set({ [currentVideo]: JSON.stringify(currentVideoBookmarks) });
+
+            response(currentVideoBookmarks);
+        }
+    });
+})();
